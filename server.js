b28eb4d6931fcf1c4a553be0b399a2bbd6b8ea88
swnb <1786718956@qq.com>
2017-12-14 19:00:27 +0800

    init 初始化项目

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const Koa = require('koa');
+const body = require('koa-body');
+const util = require('util');
 const router = require('koa-router')();
+const static = require('koa-static');
 const app = new Koa();
+const fs = require('fs');
+router.get('/', async ctx => {
+    ctx.status = 200;
+    ctx.set('content-type', 'text/html');
+    ctx.body = fs.createReadStream('./src/views/index.html');
+});
+router.post('/data', body(), async ctx => {
+    console.log(this.request.body); // if buffer or text
+    console.log(this.request.files); // if multipart or urlencoded
+    console.log(this.request.fields); // if json
+
+    ctx.status = 204;
+});
+app.use(static('./src'));
+app.use(router.routes()).listen(8080);
---
+++ src/js/index.js
@@ -0,0 +1,53 @@
+(function() {
+    navigator.getUserMedia(
+        {
+            video: true
+        },
+        function(stream) {
+            let video = document.createElement('video');
+            // video.src = window.URL.createObjectURL(stream);
+            video.srcObject = stream;
+            document.body.appendChild(video);
+            video.playing = false;
+            video.onloadeddata = function() {
+                video.play();
+                video.playing = true;
+            };
+            video.addEventListener('click', () => {
+                if (video.playing) {
+                    video.pause();
+                    video.playing = false;
+                } else {
+                    video.play();
+                    video.playing = true;
+                }
+            });
+            video.addEventListener('dblclick', () => {
+                document.body.removeChild(video);
+            });
+            let button = document.createElement('button');
+            button.innerText = 'push';
+            button.onclick = () => {
+                let canvas = document.createElement('canvas');
+                [canvas.width, canvas.height] = [
+                    video.clientWidth,
+                    video.clientHeight
+                ];
+                canvas.getContext('2d').drawImage(video, 0, 0);
+                let img = new Image();
+                img.src = canvas.toDataURL();
+                document.body.appendChild(img);
+                fetch('/data', {
+                    method: 'post',
+                    body: canvas.toDataURL()
+                }).then(res => {
+                    console.log(res);
+                });
+            };
+            document.body.appendChild(button);
+        },
+        function(err) {
+            console.log(err);
+        }
+    );
+})();
